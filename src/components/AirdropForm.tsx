573cf36af501536258f772fe44cb3c4b8bd9b135
brenda87 <bkawira10@gmail.com>
2025-10-22 19:24:35 +0300

    update

--- src/components/AirdropForm.tsx
+++ src/components/AirdropForm.tsx
@@ -2,16 +2,42 @@
 
 import { InputField } from "./ui/InputField"
 import { useState } from "react"
+import { chainsToTSender, erc20Abi, tsenderAbi } from "@/constants"
+import { useChainId, useConfig, useAccount } from "wagmi"
+import { readContract } from "@wagmi/core"
 
 export function AirdropForm() {
     const [tokenAddress, setTokenAddress] = useState("")
     const [recipients, setRecipients] = useState("")
     const [amounts, setAmounts] = useState("")
+    const chainId = useChainId()
+    const config = useConfig()
+    const account = useAccount()
+
+    async function getApproveAmount(tSenderAddress: string | null): Promise<number> {
+        if (!tSenderAddress) {
+            alert("The chain you are connected to is not supported.")
+            return 0
+        }
+        // read from the contract to see if we have approved enough tokens
+        const response = await readContract(config, {
+            abi: erc20Abi,
+            address: tokenAddress as `0x${string}`,
+            functionName: "allowance",
+            args: [account.address, tSenderAddress as `0x${string}`]
+        })
+        return response as number
+
+    }
 
     async function handleSubmit() {
-        console.log(tokenAddress)
-        console.log(recipients)
-        console.log(amounts)
+        //1a. If already approved move to step 2
+        //1b. Approve tsender contract to send our tokens
+        //2. call the airdrop function on the tsender contract
+        //3. wait for transaction to be mined
+        const tSenderAddress = chainsToTSender[chainId]["tsender"]
+        const approvedAmount = await getApproveAmount(tSenderAddress)
+        console.log(approvedAmount)
 
 
     }
@@ -38,7 +64,12 @@ export function AirdropForm() {
                 onChange={(e) => setAmounts(e.target.value)}
                 large={true}
             />
-            <button onClick={handleSubmit}>
+            <button onClick={handleSubmit}
+                className="relative px-5 py-2.5 font-semibold text-white rounded-xl 
+             bg-gradient-to-r from-purple-500 via-blue-500 to-cyan-500 
+             hover:from-cyan-500 hover:to-purple-500 
+             shadow-md hover:shadow-lg transition-all duration-300"
+            >
                 Send Airdrop
             </button>
         </div>
